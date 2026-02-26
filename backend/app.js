8de5dba5a21f2769433f172a4a8524082f58ad16
Anagha Nagaraj <anaghanag.bharadwaj@gmail.com>
2026-02-26 10:28:08 +0000

    Completed CC Lab 6 - Jenkins CI/CD with NGINX Load Balancing

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,3 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end("CC LAB 6 SUCCESS 🚀");
-});
-
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.get("/", (req, res) => {
+  res.send("Served by backend: " + process.env.HOSTNAME);
 });
